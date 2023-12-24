5b3e4d9cddd3fa126084161c38927f696694c91a
Bushra414 <126082932+Bushra414@users.noreply.github.com>
2023-12-24 11:08:14 +0200

    semi-final 2.1

--- teacher_login.php
+++ teacher_login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./sty.css">
+    <link rel="stylesheet" href="./styl.css">
 
 
     <title>Teacher Login</title>
